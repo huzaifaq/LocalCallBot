52282e2c1ff36dc079b89b0985a6ebfac51b9ad0
Sadiqali Zaveri <sadiqalizaveri@gmail.com>
2019-10-29 18:28:05 +0530

    Code cleanup

--- server.js
+++ server.js
@@ -7,9 +7,23 @@ const fs = require('fs')
 const path = require('path')
 
 const port = parseInt(process.env.PORT, 10) || 3000
-const dev = process.env.TIER === 'dev'
+const protocol = process.env.PROTOCOL || 'HTTP'
+const tier = process.env.TIER || 'dev'
+const env = process.env.NODE_ENV
+const dev = tier === 'dev'
 const app = zeitNext({ dir: '.', dev })
 const handle = app.getRequestHandler()
+
+const applicationServerLog = () => {
+	console.log(
+		'\x1b[42m\x1b[37m%s\x1b[0m',
+		' Jerjis Jewellery Store Application Started '
+	)
+	console.log('\x1b[42m\x1b[37m%s\x1b[0m', ' ENV: ', env)
+	console.log('\x1b[42m\x1b[37m%s\x1b[0m', ' TIER: ', tier)
+	console.log('\x1b[42m\x1b[37m%s\x1b[0m', ' PROTOCOL: ', protocol)
+	console.log('\x1b[42m\x1b[37m%s\x1b[0m', ' PORT: ', port)
+}
 
 app.prepare().then(() => {
 	const server = new Koa()
@@ -36,7 +50,7 @@ app.prepare().then(() => {
 	/* Main router middleware - should be used after other routes */
 	server.use(router.routes())
 
-	if (process.env.PROTOCOL === 'HTTP2') {
+	if (protocol === 'HTTP2') {
 		const cert = {
 			cert: fs.readFileSync(
 				path.join(__dirname, './certificates/localhost-cert.pem')
@@ -50,47 +64,13 @@ app.prepare().then(() => {
 		// Do not forget to browse with https://localhost:3000
 		http2.createSecureServer(cert, server.callback()).listen(port, err => {
 			if (err) throw err
-			console.log(
-				'\x1b[42m\x1b[37m%s\x1b[0m',
-				' Jerjis Jewellery Store Application Started '
-			)
-			console.log(
-				'\x1b[42m\x1b[37m%s\x1b[0m',
-				' ENV: ',
-				process.env.NODE_ENV
-			)
-			console.log(
-				'\x1b[42m\x1b[37m%s\x1b[0m',
-				' TIER: ',
-				process.env.TIER
-			)
-			console.log(
-				'\x1b[42m\x1b[37m%s\x1b[0m',
-				' PROTOCOL: ',
-				process.env.PROTOCOL
-			)
-			console.log('\x1b[42m\x1b[37m%s\x1b[0m', ' PORT: ', port)
+			applicationServerLog()
 		})
 	} else {
-		// Use this to debug the service worker
 		// Compression can to be turned on in next.config.js
 		server.listen(port, err => {
 			if (err) throw err
-			console.log(
-				'\x1b[42m\x1b[37m%s\x1b[0m',
-				' Jerjis Jewellery Store Application Started '
-			)
-			console.log(
-				'\x1b[42m\x1b[37m%s\x1b[0m',
-				' ENV: ',
-				process.env.NODE_ENV
-			)
-			console.log(
-				'\x1b[42m\x1b[37m%s\x1b[0m',
-				' TIER: ',
-				process.env.TIER
-			)
-			console.log('\x1b[42m\x1b[37m%s\x1b[0m', ' PORT: ', port)
+			applicationServerLog()
 		})
 	}
 })
