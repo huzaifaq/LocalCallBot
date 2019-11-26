eb02ab5e6b8cdf090efacbd84332dad3a7ad482a
Sadiqali Zaveri <sadiqalizaveri@gmail.com>
2019-11-26 22:35:04 +0530

    Service worker handles video files now

--- public/service-worker.js
+++ public/service-worker.js
@@ -73,4 +73,22 @@ workbox.routing.registerRoute(
 	})
 )
 
+workbox.routing.registerRoute(
+	// Cache video files.
+	/\.(?:mp4)$/,
+	// Use the cache if it's available.
+	new workbox.strategies.CacheFirst({
+		// Use a custom cache name.
+		cacheName: 'video-cache',
+		plugins: [
+			new workbox.expiration.Plugin({
+				// Cache only 20 images.
+				maxEntries: 20,
+				// Cache for a maximum of 2 days.
+				maxAgeSeconds: 2 * 24 * 60 * 60,
+			}),
+		],
+	})
+)
+
 /* eslint-disable */
