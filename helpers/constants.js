cebfad432a2e9081ab55ec0c62e93a42e193969e
Sadiqali Zaveri <sadiqalizaveri@gmail.com>
2020-01-21 19:18:11 +0530

    Sidebar timer increased to 5 seconds

--- helpers/constants.js
+++ helpers/constants.js
@@ -1,6 +1,6 @@
 export const genericErrorMsg = 'Something went wrong. Please try again later.'
 export const genericNoData = 'No results found. Please comeback later.'
 
-export const sidebarTimeout = 2000
+export const sidebarTimeout = 5000
 
 export default null
