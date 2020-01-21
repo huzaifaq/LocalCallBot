81b104cebc0d16d2f7e3ff192907381e0e110d40
Sadiqali Zaveri <sadiqalizaveri@gmail.com>
2020-01-21 19:20:53 +0530

    Add info for routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -5,8 +5,14 @@ const router = new Router({
 	prefix: '/api',
 })
 
+/**
+ * All categories are fetched
+ */
 router.get('/categories', async ctx => performDBCall(ctx, 'categories'))
 
+/**
+ * All products are fetched in a category
+ */
 router.get('/products', async ctx => performDBCall(ctx, 'products'))
 
 module.exports = router
