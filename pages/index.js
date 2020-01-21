dacf1522a05c790cdb99f3c9276b03ad08ac0cb2
Sadiqali Zaveri <sadiqalizaveri@gmail.com>
2020-01-21 19:22:27 +0530

    Add more services information on home page Move video to alternate cloudfront dns

--- pages/index.js
+++ pages/index.js
@@ -23,20 +23,37 @@ const Section = styled.section`
 	align-items: center;
 	animation: ${SlideIn} 0.3s ease-in;
 	position: relative;
+	margin-top: 20px;
+
+	&:first-child {
+		margin-top: 0px;
+	}
+
+	&:last-child {
+		margin-bottom: 50px;
+	}
+
 	@media ${props => props.theme.mobileL} {
 		min-height: 300px;
+		padding: 0px 16px;
 	}
 `
 const SectionHeading = styled.h1`
 	z-index: ${props => props.theme.elementsAboveVideoZ};
 	color: ${props => props.theme.textBlack};
 	text-align: center;
+	@media ${props => props.theme.mobileL} {
+		font-size: 20px;
+	}
 `
 
 const SectionParagraph = styled.p`
 	z-index: ${props => props.theme.elementsAboveVideoZ};
 	color: ${props => props.theme.textBlack};
 	font-size: 20px;
+	@media ${props => props.theme.mobileL} {
+		font-size: 16px;
+	}
 `
 
 const SectionVideo = styled.video`
@@ -58,7 +75,7 @@ const Index = () => {
 		<React.Fragment>
 			<Section>
 				<SectionVideo
-					src="https://d1lacrj6vu9hj5.cloudfront.net/videos/engagement-rings-nb.mp4"
+					src="https://cdn.craftedjewellers.in/videos/engagement-rings-nb.mp4"
 					autoPlay
 					muted
 					loop
@@ -66,7 +83,28 @@ const Index = () => {
 				/>
 				<SectionHeading>Welcome to Crafted Jewellers</SectionHeading>
 				<SectionParagraph>
-					One stop for all of the finest jewellery
+					One stop for all of the finest jewellery services
+				</SectionParagraph>
+			</Section>
+			<Section>
+				<SectionHeading>Precious Stones</SectionHeading>
+				<SectionParagraph>
+					Need a perfect diamond for the ring? Checkout our collection
+					of precious stones
+				</SectionParagraph>
+			</Section>
+			<Section>
+				<SectionHeading>Polishing</SectionHeading>
+				<SectionParagraph>
+					Is your jewellery looking old or needs cleaning? Polish your
+					jewellery to make it look new
+				</SectionParagraph>
+			</Section>
+			<Section>
+				<SectionHeading>Repairing</SectionHeading>
+				<SectionParagraph>
+					Does your jewellery look out of shape? Have you lost a piece
+					of the earring? Let us have a look
 				</SectionParagraph>
 			</Section>
 		</React.Fragment>
