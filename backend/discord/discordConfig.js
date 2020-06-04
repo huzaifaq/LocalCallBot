be2c2c85d2c034ce1277aa616594918fc4f3a618
Sadiqali Zaveri <sadiqalizaveri@gmail.com>
2020-06-05 02:08:44 +0530

    Remove old tokens

--- backend/discord/discordConfig.js
+++ backend/discord/discordConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
 	prefix: '!sb',
-	token: 'NzE0MDQ3MTYzNjA4MzM0NDA4.Xtfj2g.zHj8tcP9FVaqMgIO2MzCSuSuDkM',
-	channelId: '354273258033643522',
-	botId: '714047163608334408',
+	token: '',
+	channelId: '',
+	botId: '',
 }
